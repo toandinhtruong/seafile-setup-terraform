eaabce4eb6c0ec512bc4826e9f247f0cd298fa7e
unknown <dinhtoan12b1@gmail.com>
2026-03-13 12:59:57 +0700

    update

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "aws" {
   region = var.aws_region
   default_tags {
     tags = {
-      Environment = var.environment
+      Environment = "dev"
       ManagedBy   = "Terraform"
       Client      = "Tom"
     }
